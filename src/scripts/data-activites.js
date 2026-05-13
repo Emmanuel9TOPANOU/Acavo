e4edd6c49987aca551335f34e4b058758055e822
Judicaël TOPANOU <j703066@gmail.com>
2026-05-13 10:03:05 +0100

    Ajout du fichier volumineux

---
+++ src/scripts/data-activites.js
@@ -0,0 +1,67 @@
+const baseActivites = {
+    "1": {
+        titre: "Distribution des dons en présences des veuves à l’Eglise Evangélique les Assemblées de Dieu (Maranatha)",
+        description: "À l’occasion des périodes festives, ACAVO organise des distributions de dons en faveur des veuves et des personnes en situation d’affliction, notamment à l’Église Évangélique les Assemblées de Dieu (Maranatha). Ces moments de partage visent à alléger leurs charges et à leur offrir un peu de réconfort dans des périodes souvent marquées par des difficultés financières et émotionnelles..",
+        objectifs: "Apporter un soutien matériel et moral aux veuves et aux personnes vulnérables pendant les fêtes, afin de réduire leur stress et leur permettre de vivre ces moments dans plus de sérénité et de dignité.",
+        actions: "ACAVO a procédé à une distribution de vivres et de dons en présence des bénéficiaires au sein de l’église Maranatha. Cette action s’est déroulée dans une ambiance conviviale et solidaire, favorisant les échanges, le soutien moral et le partage entre les membres de la communauté et les bénéficiaires.",
+        impact: "Cette initiative a permis de soulager les bénéficiaires face aux charges liées aux périodes festives, tout en leur redonnant le sourire. Elle a également renforcé le sentiment de solidarité et apporté du réconfort à des personnes traversant des moments difficiles.",
+        images: ["src/assets/images/img2.png", "src/assets/images/img23.png"]
+    },
+    "2": {
+        titre: "Distribution des vivres à l’Eglise Evangélique les Assemblées de Dieu (Natitingou)",
+        description: "ACAVO a effectué une mission à Natitingou, au sein de l’Église Évangélique les Assemblées de Dieu, où une distribution de vivres a été organisée au profit des veuves et des orphelins. Cette rencontre, marquée par une forte émotion, a rassemblé de nombreux bénéficiaires dans un esprit de partage et de solidarité.",
+        objectifs: "Apporter un soutien concret aux veuves et aux orphelins tout en créant un cadre d’échange et d’écoute visant à renforcer leur espoir et leur accompagnement.",
+        actions: "Lors de cette activité, le Président de l’ONG a pris la parole pour partager sa vision et exprimer son engagement en faveur des personnes en situation de précarité. En présence du pasteur de l’église, des échanges sincères et enrichissants ont eu lieu avec les bénéficiaires. La rencontre s’est clôturée par une distribution de vivres et de vêtements aux veuves présentes.",
+        impact: "Cette initiative a permis de répondre à des besoins essentiels tout en apportant un soutien moral important aux bénéficiaires. Elle a également renforcé les liens entre l’ONG, les responsables religieux et la communauté locale, favorisant ainsi un climat de confiance et de solidarité durable.",
+        imagePrincipale: "src/assets/images/img3.png",
+        images: ["src/assets/images/img3.png" , "src/assets/images/img24.png"]
+    },
+    "3": {
+        titre: "Distribution des fournitures scolaires aux orphelins à l’Eglise Evangélique les Assemblées de Dieu (Bah Mora)",
+        description: "À l’Église Évangélique les Assemblées de Dieu (Bah Mora), ACAVO a organisé une distribution de fournitures scolaires au profit des enfants orphelins issus de familles de veuves. Cette initiative vise à les accompagner dans le démarrage de l’année scolaire dans de meilleures conditions.",
+        objectifs: "Soutenir la scolarisation des orphelins en leur fournissant les outils nécessaires pour bien débuter l’année scolaire.",
+        actions: "L’ONG a procédé à la remise de sacs d’écoliers contenant des fournitures scolaires essentielles à plusieurs enfants orphelins de la communauté. Cette action s’est déroulée dans un cadre solidaire, en présence des responsables de l’église et des bénéficiaires.",
+        impact: "Cette initiative a permis de soulager les familles en difficulté tout en motivant les enfants à poursuivre leur parcours scolaire avec plus de sérénité. Elle contribue également à renforcer l’accès à l’éducation pour les plus vulnérables.",
+        images: ["src/assets/images/img4.png"]
+    },
+    "4": {
+        titre: "Distribution des fournitures scolaires aux orphelins à l’Eglise Evangélique les Assemblées de Dieu (Kobrikonto)",
+        description: "À Kobrikonto, dans l’arrondissement de Sanson (commune de Tchaourou), ACAVO a organisé une rencontre avec des veuves et des orphelins au sein de l’Église Évangélique les Assemblées de Dieu. Cette activité a été marquée par une distribution de fournitures scolaires destinée aux enfants orphelins.",
+        objectifs: "Accompagner les orphelins dans leur parcours scolaire tout en apportant un soutien moral et social aux familles concernées.",
+        actions: "En présence du pasteur de l’église, l’ONG a échangé avec les bénéficiaires dans un cadre convivial et bienveillant. Des kits scolaires ont ensuite été remis aux enfants orphelins afin de les aider à mieux aborder leur année académique.",
+        impact: "Cette initiative a permis de soutenir les enfants dans leur scolarité et de renforcer le lien entre l’association et la communauté locale. Elle a également apporté du réconfort aux familles, tout en encourageant les enfants à poursuivre leurs études avec motivation.",
+        images: ["src/assets/images/img5.png"]
+    },
+    "5": {
+        titre: "Installation du bureau ACAVO au soutien de l’Eglise Evangélique des Assemblées de Dieu de Sansou  ",
+        description: "L’Église Évangélique des Assemblées de Dieu de Sanson a servi de cadre à l’installation du bureau relais d’ACAVO. Cette activité s’est tenue en présence des veuves des différentes églises de l’arrondissement, dans une ambiance marquée par l’intérêt et l’adhésion des participantes.",
+        objectifs: "Renforcer la présence locale de l’ONG afin de mieux coordonner ses actions et se rapprocher des bénéficiaires.",
+        actions: "Au cours de cette rencontre, le bureau relais d’ACAVO a été officiellement mis en place. Des échanges ont eu lieu avec les participantes, permettant de présenter les objectifs de l’association et de répondre aux différentes préoccupations. Les discussions, ouvertes et constructives, ont favorisé une meilleure compréhension des actions de l’ONG.",
+        impact: "Cette installation marque une étape importante dans le développement d’ACAVO sur le terrain. Elle facilite la mise en œuvre des activités locales, renforce la proximité avec les bénéficiaires et encourage une meilleure implication des communautés.",
+        images: ["src/assets/images/img11.jpg"]
+    },
+    "6": {
+        titre: "Distribution des fournitures scolaires aux orphelins à l’Eglise Evangélique les Assemblées de Dieu (Tourou)",
+        description: "À l’Église Évangélique les Assemblées de Dieu de Tourou, ACAVO a procédé à une distribution de fournitures scolaires au profit des enfants orphelins. Cette action a permis d’accompagner plusieurs apprenants dans leur préparation à l’année scolaire.",
+        objectifs: "Soutenir les orphelins dans leur scolarité en leur fournissant les équipements nécessaires pour bien démarrer l’année académique.",
+        actions: "Une dizaine d’enfants, principalement des orphelins, ont reçu des kits scolaires composés de sacs et de fournitures essentielles. La remise s’est déroulée en présence des tuteurs et des responsables locaux, dans un climat de partage et de solidarité.",
+        impact: "Cette initiative a permis de soulager les familles tout en renforçant la motivation des enfants à poursuivre leur scolarité. Les bénéficiaires et leurs tuteurs ont exprimé leur gratitude face à ce geste qui a apporté joie et encouragement.",
+        images: ["src/assets/images/img5.png"]
+    },
+    "7": {
+        titre: "Rencontre avec les veuves et orphelins de l’Eglise Evangélique les Assemblées de Dieu (N’Dali)",
+        description: "À N’Dali, ACAVO a organisé une rencontre avec des veuves et des orphelins afin de leur présenter la vision, l’organisation et le fonctionnement de l’association. Cette activité a également été marquée par une distribution de dons au profit des bénéficiaires, y compris dans des localités voisines comme Bamkpo.",
+        objectifs: "Sensibiliser les communautés locales aux actions de l’ONG tout en renforçant leur engagement et en apportant un soutien concret aux personnes vulnérables..",
+        actions: "Lors de cette rencontre, les responsables d’ACAVO ont échangé avec les participants pour expliquer les objectifs et les missions de l’organisation. Les bénéficiaires ont exprimé leur volonté de s’impliquer dans la mise en place du bureau relais. L’activité s’est poursuivie par une distribution de dons aux veuves et aux orphelins présents, ainsi qu’à d’autres bénéficiaires dans les villages environnants.",
+        impact: "Cette initiative a permis de renforcer la compréhension et l’adhésion des communautés locales aux actions de l’ONG. Elle a également contribué à instaurer un climat de confiance, tout en apportant un soutien matériel et moral aux bénéficiaires.",
+        images: ["src/assets/images/img10.png"]
+    },
+    "8": {
+        titre: "Soutien d’une famille ayant perdu un proche dans un accident à  l’Eglise Evangélique les Assemblées de Dieu (Bampko) ",
+        description: "À Bamkpo, dans la commune de N’Dali, ACAVO a mené une action de soutien auprès d’une famille ayant perdu un proche dans un accident de circulation. Cette intervention s’inscrit dans une dynamique d’accompagnement humain et solidaire des personnes touchées par des situations difficiles.",
+        objectifs: "Apporter un soutien moral et matériel aux familles endeuillées, tout en renforçant la présence et l’accompagnement de l’ONG au sein des communautés locales.",
+        actions: "À l’occasion de rencontres organisées dans la localité, ACAVO a également procédé à la mise en place d’un bureau relais chargé de faciliter le suivi des actions sur le terrain. La famille concernée a bénéficié d’un accompagnement et d’un soutien de la part de l’organisation. Depuis, elle continue de recevoir une attention particulière de la part des responsables de l’ONG.",
+        impact: "Réconfort apporté à une famille en détresse et renforcement des liens de solidarité.",
+        images: ["src/assets/images/img9.png"]
+    }
+};
